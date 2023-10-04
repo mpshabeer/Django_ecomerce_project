91f705051464269075e98fd9e6e1ccd466711613
mpshabeer <mpshabeer8182@gmail.com>
2023-10-04 13:40:43 +0530

    final

--- ecomerce/shop/context_processors.py
+++
@@ -1,5 +0,0 @@
-from .models import Catogory
-
-def menu_links(request):
-    links=Catogory.objects.all()
-    return dict(links=links)
--- ecomerce/shop/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-app_name='shop'
-urlpatterns = [
-    path('',views.allProductCat,name="allProductCat"),
-    path('<slug:c_slug>/',views.allProductCat,name="products_by_catogory"),
-    path('<slug:c_slug>/<slug:product_slug>/', views.productdetails, name='productdetails'),
-    ]
